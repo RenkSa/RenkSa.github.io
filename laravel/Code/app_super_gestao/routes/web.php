358ea0159b72d7a65e7417111ce1b53ba8761014
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-06-18 22:08:40 -0300

    alicando controllers

--- laravel/Code/app_super_gestao/routes/web.php
+++ laravel/Code/app_super_gestao/routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return 'Home';
+// });
+// Route::get('/sobre-nós', function () {
+//     return 'sobre-nós';
+// });
+
+// Route::get('/contato', function () {
+//     return '';
+// });
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+
