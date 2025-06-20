9e0b101b6a00b1b7e5190653036f8a66692dcb7a
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-06-20 15:39:13 -0300

    envio de parametros nas rotas

--- laravel/Code/app_super_gestao/routes/web.php
+++ laravel/Code/app_super_gestao/routes/web.php
@@ -18,5 +18,10 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+// nome, categoria, assunto, mensagem
 
 
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) {
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem ";
+
+});
