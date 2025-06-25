02aebb965664fddfdc7d51cb44511d187d49c7e9
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-06-25 14:30:40 -0300

    tratando parametros de rotas com expressoes regulares

--- laravel/Code/app_super_gestao/routes/web.php
+++ laravel/Code/app_super_gestao/routes/web.php
@@ -22,15 +22,13 @@ Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato
 
 
 Route::get(
-    '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', 
+    '/contato/{nome}/{categoria_id}', 
     function(
         string $nome = 'Desconhecido', 
-        string $categoria = 'Informação',
-        string $assunto = 'Contato',
-        string $mensagem = 'mensagem não informada'
+        int $categoria_id = 1
 
         ) {
 
-            echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem ";
+            echo "Estamos aqui: $nome - $categoria_id";
 
-});
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
