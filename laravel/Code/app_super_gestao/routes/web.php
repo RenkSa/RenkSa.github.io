fb9738d2c402c3a1000a2e32a2a1de5656b2f47d
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-06-25 16:11:19 -0300

    agrupando rotas

--- laravel/Code/app_super_gestao/routes/web.php
+++ laravel/Code/app_super_gestao/routes/web.php
@@ -24,15 +24,22 @@ Route::get('/login', function(){
     return 'Login';
 });
 
-Route::get('/clientes', function(){
+Route::prefix('/app')->group(function(){
+    
+    Route::get('/clientes', function(){
     return 'Clientes';
 });
 
-Route::get('/fornecedores', function(){
+    Route::get('/fornecedores', function(){
     return 'Fornecedores';
 });
 
-Route::get('/produtos', function(){
+    Route::get('/produtos', function(){
     return 'Produtos';
 });
+
+
+});
+
+
 
