671b38d4281dc7f90db95de8e4bddb95167a9528
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-07-09 23:51:59 -0300

    Blade - @if/@else

--- laravel/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ laravel/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,11 +3,28 @@
 {{ 'Texto de Teste' }}
 <?= 'texto de teste' ?>
 
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+{{-- Fica o comentário que será descartado pelo interpretador do blade / na sintaxe do blade nao utilizamos os ';' para encerrar os comandos! --}}
 
 
 
 <script>
     document.body.style.background = '#000'
     document.body.style.color = '#fff'
+    /*
+    if() {
+        
+    } else if() {
+
+    } else {
+        
+    } */
+
 </script>
+
+@if(count($fornecedores) > 0 && count($fornecedores) <= 10)
+    <h3> Existem alguns fornecedores cadastrados </h3>
+@elseif(count($fornecedores) > 10)
+    <h3> Existem vários fornecedores cadastrados </h3>
+@else
+    <h3> Ñ Existem fornecedores cadastrados </h3>
+@endif
