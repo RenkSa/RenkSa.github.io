e0ba264dfd2a4bfbdd5dfddae38cc864159db4bb
Renk Willy da Mota Sá <renkwiii@outlook.com>
2025-07-10 21:14:36 -0300

    Blade - @unless

--- laravel/Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ laravel/Code/app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,10 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = ['Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1'];
+        // $fornecedores = ['Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Fornecedor 1','Forneced
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'S']
+        ];
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- laravel/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ laravel/Code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,6 @@
 <h3>Fornecedor</h3>
 
-{{ 'Texto de Teste' }}
-<?= 'texto de teste' ?>
+{{-- {{ 'Texto de Teste' }}
 
 {{-- Fica o comentário que será descartado pelo interpretador do blade / na sintaxe do blade nao utilizamos os ';' para encerrar os comandos! --}}
 
@@ -21,10 +20,25 @@
 
 </script>
 
-@if(count($fornecedores) > 0 && count($fornecedores) <= 10)
+{{-- @unless executa se o retorno for false --}}
+<br>
+Fornecedor: {{ $fornecedores[0] ['nome'] }}
+<br>
+Status: {{ $fornecedores[0] ['status'] }}
+<br>
+@if($fornecedores[0]['status'] == 'S')
+    Fornecedor inativo.
+@endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condicao for false -->
+    Fornecedor inativo.
+@endunless
+
+
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) <= 10)
     <h3> Existem alguns fornecedores cadastrados </h3>
 @elseif(count($fornecedores) > 10)
     <h3> Existem vários fornecedores cadastrados </h3>
 @else
     <h3> Ñ Existem fornecedores cadastrados </h3>
-@endif
+@endif --}}
